84442e5b79c85f7d425069665a17bf236b35b608
sumitpati7 <sumitpati7@gmail.com>
2025-01-18 16:18:58 +0545

    Image and space with user

--- server.js
+++ server.js
@@ -96,5 +96,19 @@ app.get('/spaces/:filename', (req, res) => {
   });
 });
 
+app.get('/profile/:filename', (req, res) => {
+  // console.log(req.params.filename)
+  const filePath = path.join(__dirname,'uploads', req.params.filename);
+  console.log(filePath)
+
+  // Send the file
+  res.sendFile(filePath, (err) => {
+      if (err) {
+          console.error('Error sending file:', err);
+          res.status(404).send('File not found');
+      }
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
