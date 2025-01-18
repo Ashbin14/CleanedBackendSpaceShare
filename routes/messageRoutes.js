238c620867d6db4c899723cf99db732a29bf7c53
Luja Chitrakar <lujachitrakar156@gmail.com>
2025-01-19 02:19:08 +0545

    chat-bot fixed

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -5,13 +5,13 @@ import {
   getMessages,
   getUsersForSidebar,
   sendMessage,
+  getChatList,
 } from "../controllers/messageController.js";
 
 const router = express.Router();
 
 router.get("/users", authenticate, getUsersForSidebar);
+router.get("/chatlist", authenticate, getChatList); // Place this before "/:id"
 router.get("/:id", authenticate, getMessages);
-
 router.post("/send/:id", authenticate, sendMessage);
-
 export default router;
